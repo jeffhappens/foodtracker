8ca86ee3d96522db07e40e0165564dcfca6c3385
Jeff Mills <jeffrey.mills@outlook.com>
2014-12-11 09:57:18 -0600

    Text edits

--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h3>Food Tracker Application <button class="btn btn-primary pull-right addMeal">New Meal</button></h3>
+				<h3>Food Tracker <button class="btn btn-primary pull-right addMeal">New Meal</button></h3>
 			</div>
 		</div>
 	</div>
